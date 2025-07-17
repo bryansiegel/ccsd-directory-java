8fac7df6bc7a9045c41745c54801c4fa482ef9e7
Bryan Siegel <bryansiegel@gmail.com>
2025-07-17 08:14:11 -0700

    refactor design

--- src/main/java/com/ccsd/ccsddirectoryjava/controllers/AuthController.java
+++ src/main/java/com/ccsd/ccsddirectoryjava/controllers/AuthController.java
@@ -1,10 +1,23 @@
 package com.ccsd.ccsddirectoryjava.controllers;
 
+import com.ccsd.ccsddirectoryjava.repositories.ContactRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+
+
+
+
 public class AuthController {
+
+    @Autowired
+    private ContactRepo contactRepo;
+
+
         @GetMapping("/login")
         public String login() {
             return "admin/login";
@@ -16,7 +29,9 @@ public class AuthController {
         }
 
         @GetMapping("admin/dashboard/")
-        public String dashboard() {
+        public String dashboard(Model model) {
+            model.addAttribute("contacts", contactRepo.findAll(PageRequest.of(0, 5)).getContent());
+
             return "admin/dashboard";
         }
 
